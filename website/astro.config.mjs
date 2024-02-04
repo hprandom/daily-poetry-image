3de3112edb71a4dabb5bbeff2b2af9d1d8671c78
hprandom <app@hp.email>
2024-02-04 13:34:59 +0800

    Update astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
-import transitions from "@astrojs/transitions";
 
 import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: SITE.website,
-    integrations: [tailwind(), sitemap(),transitions()],
+    integrations: [tailwind(), sitemap()],
     output: "static",
     adapter: vercel({
         webAnalytics: {
